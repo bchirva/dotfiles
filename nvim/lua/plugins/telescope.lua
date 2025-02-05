0b96ba680f55da57167ca8e93dc16877f034038d
b_chirva <bchirva@gmail.com>
2025-02-05 14:45:49 +0500

    Nvim: telescope search from PWD

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,8 +16,10 @@ telescope.setup {
 }
 
 local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
-vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
+vim.api.nvim_set_keymap('n', '<leader>ff',
+    "<cmd>lua require('telescope.builtin').find_files({cwd = os.getenv('PWD')})<cr>", opts)
+vim.api.nvim_set_keymap('n', '<leader>fg',
+    "<cmd>lua require('telescope.builtin').live_grep({cwd = os.getenv('PWD')})<cr>", opts)
 vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 vim.api.nvim_set_keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
 
