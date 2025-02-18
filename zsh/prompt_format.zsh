5694326317be0d5de970f14d1247e348f5a83ecc
b_chirva <bchirva@gmail.com>
2025-02-18 19:31:36 +0500

    Zsh: prompt fixes

--- zsh/prompt_format.zsh
+++ zsh/prompt_format.zsh
@@ -17,20 +17,23 @@ function git_prompt_block() {
         exit
     fi
 
-    BRANCH=$(git status --branch 2>/dev/null | head -n 1 | awk '{print $NF}')
+    local BRANCH=$(git status --branch 2>/dev/null | head -n 1 | awk '{print $NF}')
     if [ ! -z "$BRANCH" ]; then BRANCH="${GIT_BRANCH_CHAR} ${BRANCH}"; else BRANCH=""; fi
 
-    TAG=$(git tag --points-at)
+    local TAG=$(git tag --points-at 2>/dev/null)
     if [ ! -z "$TAG" ]; then TAG=" (${GIT_TAG_CHAR} ${TAG})"; else TAG=""; fi
 
-    if [ "$(git status --short 2>/dev/null)" ]; then CHANGES=" (${GIT_DIFF_CHAR})"; else CHANGES=""; fi
+    local CHANGES=
+    if [ "$(git status --short 2>/dev/null)" ]; then 
+        CHANGES=" (${GIT_DIFF_CHAR})"; else CHANGES=""
+    fi
 
     echo -en "${BRANCH}${TAG}%{$fg_bold[${ERROR_COLOR_NAME}]%}${CHANGES}%{$fg[${WARNING_COLOR_NAME}]%} ${SEPARATOR_CHAR} "
 }
 
 function venv_prompt_block() {
     if [[ "$VIRTUAL_ENV" ]]; then
-        echo -en "${PYTHON_CHAR} $(basename ${VIRTUAL_ENV}) ${SEPARATOR_CHAR} "
+        echo -en "${PYTHON_CHAR} $(basename ${VIRTUAL_ENV}) ($(python --version | awk '{print $NF}')) ${SEPARATOR_CHAR} "
     fi
 }
 
