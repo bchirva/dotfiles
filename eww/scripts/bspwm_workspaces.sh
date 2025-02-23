f7deb41a409ecadd7fea0bb730d262d4225d2e64
b_chirva <bchirva@gmail.com>
2025-02-24 01:35:11 +0500

    Eww: bspwm workspaces component for multiple monitors

--- eww/scripts/bspwm_workspaces.sh
+++ eww/scripts/bspwm_workspaces.sh
@@ -7,7 +7,7 @@ case $operation in
         target_monitor=$2
         bspc subscribe desktop monitor report | while read -r _; do
             workspaces=()
-            readarray -t workspaces <<< $(bspc query -D -m $target_monitor)
+            readarray -t workspaces <<< $(bspc query -D)
 
             result="[]"
             for workspace in "${workspaces[@]}"
@@ -18,9 +18,10 @@ case $operation in
                 elif [[ "$(bspc query -D -d .occupied | grep "$workspace")" ]]; then
                     state="active"
                 fi
-                result=$(echo $result | jq ". + [{id: \"$workspace\", state: \"$state\"}]")
+                monitor=$(bspc query -M --names -d $workspace)
+                result=$(echo $result | jq ". + [{id: \"$workspace\", state: \"$state\", monitor: \"$monitor\"}]")
             done
-            echo $result
+            echo $result 
         done
         ;;
     "switch")
