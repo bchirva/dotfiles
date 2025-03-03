5ce9da572360f1ba906046578eaf09dfd91e723a
b_chirva <bchirva@gmail.com>
2025-03-03 15:37:17 +0500

    Nvim: remove symbols_outline.lua; add Nerd-icons as trouble diagnostics signs

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -6,3 +6,12 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>sd', '<cmd>Trouble diagnostics toggle<CR>', opts)
 vim.keymap.set('n', '<leader>ss', '<cmd>Trouble symbols toggle focus=true<CR>', opts)
 vim.keymap.set('n', '<leader>sl', '<cmd>Trouble lsp toggle<CR>', opts)
+
+
+local signs = { Error = " ", Warn = " ", Info = " ", Hint = "󰅺" }
+-- local signs = { Error = " ", Warn = " ", Info = " ", Hint = "" }
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
