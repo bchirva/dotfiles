ad712c98dde12c9f4f740ddffb653ecb4184da58
b_chirva <bchirva@gmail.com>
2025-05-04 17:53:15 +0500

    Nvim: DAP for python

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -52,7 +52,7 @@ return {
 			else
 				cb({
 					type = "executable",
-					command = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python",
+					command = vim.fn.expand("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"),
 					args = { "-m", "debugpy.adapter" },
 					options = {
 						source_filetype = "python",
@@ -92,13 +92,29 @@ return {
 			{
 				type = "python",
 				request = "launch",
-				name = "Launch file",
+				name = "Launch current file",
 
 				program = "${file}",
 				pythonPath = function()
 					local venv = os.getenv("VIRTUAL_ENV")
 					if venv then
-						return venv .. "/python"
+						return venv .. "/bin/python"
+					else
+						return "/usr/bin/python"
+					end
+				end,
+			},
+			{
+				type = "python",
+				request = "launch",
+				name = "Launch module",
+				module = function()
+					return vim.fn.input("Module name: ")
+				end,
+				pythonPath = function()
+					local venv = os.getenv("VIRTUAL_ENV")
+					if venv then
+						return venv .. "/bin/python"
 					else
 						return "/usr/bin/python"
 					end
