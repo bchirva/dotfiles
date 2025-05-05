43bff713df8908bce81b697386ed22a2a73619de
b_chirva <bchirva@gmail.com>
2025-05-06 01:56:08 +0500

    Yazi: update config; add git and copy-file-content plugins

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,5 +1,10 @@
-local colors = require("colors")
+require("full-border"):setup({
+	type = ui.Border.ROUNDED,
+})
 
-require("full-border"):setup {
-    type = ui.Border.ROUNDED
-}
+require("git"):setup()
+
+require("copy-file-contents"):setup({
+	append_char = "\n",
+	notification = true,
+})
