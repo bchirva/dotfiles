cb31b69b665850d2ee2a30cc8325f30c055d767c
b_chirva <bchirva@gmail.com>
2025-05-25 00:37:47 +0500

    Rofi: add colorschemes picker entry to master menu

--- rofi/modules/rofi_master.sh
+++ rofi/modules/rofi_master.sh
@@ -9,12 +9,13 @@ function main() {
         echo -en "$(colored-icon pango 󰖟 ) Network\n"
         echo -en "$(colored-icon pango 󰂯 ) Bluetooth\n"
         echo -en "$(colored-icon pango  ) System monitor\n"
+        echo -en "$(colored-icon pango  "${WARNING_COLOR}") Colorschemes picker\n"
         echo -en "$(colored-icon pango  "${ERROR_COLOR}") Logout\n"
     }
 
     local -r variant=$(rofi_entries \
         | rofi -config "$HOME/.config/rofi/modules/controls_config.rasi" \
-        -markup-rows -i -dmenu -no-custom -format 'i' -p "Control Center" -l 6)
+        -markup-rows -i -dmenu -no-custom -format 'i' -p "Control Center" -l 7)
 
     case $variant in 
         0) $HOME/.config/rofi/modules/rofi_audio.sh output ;;
@@ -22,7 +23,8 @@ function main() {
         2) $HOME/.config/rofi/modules/rofi_network.sh main;;
         3) $HOME/.config/rofi/modules/rofi_bluetooth.sh main;;
         4) kitty -e btm ;;
-        5) $HOME/.config/rofi/modules/rofi_powermenu.sh ;;
+        5) $HOME/.config/rofi/modules/rofi_colorscheme.sh ;;
+        6) $HOME/.config/rofi/modules/rofi_powermenu.sh ;;
         *) exit ;;
     esac
 }
