3ecf3f4ab8823564c08d22eb5e4fdd4f14ff692e
b_chirva <bchirva@gmail.com>
2025-06-17 00:24:01 +0500

    Screenshot: add options (incl. for rofi) to screenshot selected screen

--- rofi/modules/rofi_screenshot.sh
+++ rofi/modules/rofi_screenshot.sh
@@ -4,18 +4,50 @@ source $HOME/.config/theme.sh
 
 function main() {
     function rofi_input() {
-        echo -en "$(colored-icon pango 󰹑 ) ...selected area or window\n"
-        echo -en "$(colored-icon pango 󰹑 ) ...focused window in 3 sec\n"
+        echo -en "$(colored-icon pango 󰒉 ) ...selected area or window\n"
+        echo -en "$(colored-icon pango 󰇀 ) ...focused window\n"
+        echo -en "$(colored-icon pango 󰍹 ) ...screen\n"
     }
 
     local -r variant=$(rofi_input \
         | rofi -config "$HOME/.config/rofi/modules/controls_config.rasi" \
-        -markup-rows -i -dmenu -p "Screenshot of..." -format "i" -l 2 -no-custom)
+        -markup-rows -no-custom -i -dmenu \
+        -p "Screenshot of..." \
+        -format "i" \
+        -l 3)
+
 
     if [[ $variant ]]; then
         case $variant in
-            0) screenshot-cmd select ;;
-            1) screenshot-cmd focused ;;
+            0) 
+                sleep 0.5
+                screenshot-cmd select ;;
+            1) 
+                sleep 0.5
+                screenshot-cmd focused ;;
+            2) 
+                readarray -t screens <<< "$(xrandr \
+                    | grep "\bconnected\b" \
+                    | awk '{print $1}')"
+
+                local rofi_input_screens
+                for screen in "${screens[@]}"; do 
+                    rofi_input_screens="${rofi_input_screens}$(colored-icon pango 󰍹 ) ${screen}\n"
+                done 
+
+                local -r variant_screen=$(echo -en "${rofi_input_screens}" \
+                    | rofi -config "${XDG_CONFIG_HOME}/rofi/modules/controls_config.rasi" \
+                        -markup-rows -no-custom -i -dmenu \
+                        -p "Screenshot of..." \
+                        -format "i" \
+                        -l ${#screens[@]})
+
+                if [[ $variant_screen ]]; then 
+                    local -r selected_screen="${screens[${variant_screen}]}"
+                    sleep 0.5
+                    screenshot-cmd screen "${selected_screen}"
+                fi 
+                ;;
             *) exit ;;
         esac
     fi
