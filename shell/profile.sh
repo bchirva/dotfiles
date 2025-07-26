4fe19d0e75a2c5950e5fce4e6ee46b4a5d68a6d1
b_chirva <bchirva@gmail.com>
2025-07-26 11:56:09 +0500

    Profiles: zprofile & xprofile sources shell/profile.sh

--- shell/profile.sh
+++ shell/profile.sh
@@ -6,8 +6,6 @@ export XDG_DATA_HOME=$HOME/.local/share
 export XDG_RUNTIME_DIR=/run/user/$UID
 export XDG_STATE_HOME=$HOME/.local/state
 
-export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
-
 export CARGO_HOME=$XDG_DATA_HOME/cargo
 export CONAN_HOME=$XDG_DATA_HOME/conan2
 export DEVPOD_HOME=$XDG_CACHE_HOME/devpod
@@ -16,7 +14,6 @@ export FFMPEG_DATADIR=$XDG_DATA_HOME/ffmpeg
 export FZF_DEFAULT_OPTS_FILE=$XDG_CONFIG_HOME/fzf/fzfrc
 export GOMODCACHE=$XDG_CACHE_HOME/go/mod
 export GOPATH=$XDG_DATA_HOME/go
-export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc":"$XDG_CONFIG_HOME/gtk-2.0/gtkrc.mine"
 export MACHINE_STORAGE_PATH=$XDG_DATA_HOME/docker/docker-machine
 export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
 export NVM_DIR=$XDG_DATA_HOME/nvm
@@ -31,12 +28,6 @@ export SSH_ASKPASS=rofi-askpass
 export STARSHIP_CACHE=$XDG_CACHE_HOME/starship/cache
 export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/config.toml
 
-export BROWSER="zen"
 export EDITOR="nvim"
-export TERMINAL="kitty"
-export VISUAL="nvim"
-
-export QT_QPA_PLATFORMTHEME=qt5ct
-export QT_STYLE_OVERRIDE=kvantum
 
 [ -f "$HOME/.private_env" ] && . "$HOME/.private_env"
---
+++ zprofile
@@ -0,0 +1,4 @@
+[ -f "$HOME/.config/shell/profile.sh" ] && . "$HOME/.config/shell/profile.sh"
+
+export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+
