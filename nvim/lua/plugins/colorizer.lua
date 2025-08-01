464df6ba57fbead70cf271cb0f854b33983546ce
b_chirva <bchirva@gmail.com>
2025-08-01 14:00:25 +0500

    Nvim: colorizer highlight rgb() in css

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,17 +1,20 @@
 return {
-    "norcalli/nvim-colorizer.lua",
-    config = function ()
-        require("colorizer").setup({})
+	"norcalli/nvim-colorizer.lua",
+	config = function()
+		require("colorizer").setup({
+			css = {
+				rgb_fn = true,
+			},
+		})
 
-        vim.api.nvim_create_autocmd({"BufReadPost", "BufNewFile"}, {
-            callback = function()
-                vim.cmd("ColorizerAttachToBuffer")
-            end
-        })
+		vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+			callback = function()
+				vim.cmd("ColorizerAttachToBuffer")
+			end,
+		})
 
-        vim.keymap.set("n", "<leader>uc", function()
-            vim.cmd("ColorizerToggle")
-        end, {desc = "Colorizer"} )
-
-    end
+		vim.keymap.set("n", "<leader>uc", function()
+			vim.cmd("ColorizerToggle")
+		end, { desc = "Colorizer" })
+	end,
 }
