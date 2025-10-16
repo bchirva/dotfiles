c6137f3ffd5318673e0e528243f9bdba344a61e3
b_chirva <bchirva@gmail.com>
2025-10-16 13:05:52 +0500

    Nvim: config avante with local ollama only

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,24 +1,32 @@
-local avante = {
-	"yetone/avante.nvim",
-	event = "VeryLazy",
-	version = false,
 
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"nvim-telescope/telescope.nvim",
-		"hrsh7th/nvim-cmp",
-		"folke/snacks.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-    opts = {}
-}
+local is_ollama_running = os.execute("pgrep ollama > /dev/null") == 0
+if is_ollama_running then
+    return {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        version = false,
 
-local openai_key = os.getenv("OPENAI_API_KEY")
-if openai_key ~= nil then
-	avante.opts.provider = "openai"
-	return avante
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-telescope/telescope.nvim",
+            "hrsh7th/nvim-cmp",
+            "folke/snacks.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            provider = "ollama",
+            input = {
+                provider = "snacks",
+                provider_opts = {
+                    title = "Avante Input",
+                    icon = " ",
+                },
+            },
+        },
+    }
+else
+    return {}
 end
 
-return {}
