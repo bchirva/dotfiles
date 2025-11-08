60a467102be2b226ec51f8b79cc641d27ecabad5
b_chirva <bchirva@gmail.com>
2025-11-08 20:25:55 +0500

    Shell: fix autostart scripts (run only if command exist)

--- shell/tmux-autostart.sh
+++ shell/tmux-autostart.sh
@@ -16,11 +16,15 @@ function _tmux_autostart() {
     fi
 }
 
-export PATH="${HOME}/.config/tmux/plugins/tmuxifier/bin:${PATH}"
-eval "$(tmuxifier init -)"
 
-if [[ -z "$TMUX" && -z "$VIM" ]]; then
-    if ! tmux has-session 2>/dev/null; then
-        _tmux_autostart 
+if command -v tmux >/dev/null ; then 
+	export PATH="${HOME}/.config/tmux/plugins/tmuxifier/bin:${PATH}"
+
+    if command -v tmuxifier >/dev/null; then
+    	eval "$(tmuxifier init -)"
     fi
+
+	if [ -z "$TMUX" ] && [ -z "$VIM" ] && [ ! tmux has-session 2>/dev/null ]; then
+		_tmux_autostart 
+	fi
 fi
--- shell/version_managers.sh
+++ shell/version_managers.sh
@@ -1,5 +1,5 @@
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  
 
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+[ -d $PYENV_ROOT/bin ] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --detect-shell -)"
