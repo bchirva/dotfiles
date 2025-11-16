1c649698690878399c57c93249bedfd3e5ae1ffa
b_chirva <bchirva@gmail.com>
2025-11-16 17:04:06 +0500

    scripts: POSIX-sh rofi_screen_capture.sh

--- rofi/bin/rofi_screen_capture.sh
+++ rofi/bin/rofi_screen_capture.sh
@@ -1,78 +1,74 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-source "${XDG_CONFIG_HOME}/shell/theme.sh"
+. "${XDG_CONFIG_HOME}/shell/theme.sh"
 
-function main() {
-    function rofi_input() {
-        echo -en "$(colored-icon pango 󰇀 ) Shot focused window\n"
-        echo -en "$(colored-icon pango 󰍹 ) Shot whole screen\n"
-        echo -en "$(colored-icon pango 󰒉 ) Shot selected area or window\n"
-        echo -en "$(colored-icon pango  ) Record screen\n"
-    }
+select_screen() {
+    screens="$(xrandr --listactivemonitors \
+        | sed 1d \
+        | awk '{print $NF}')"
 
-    function select_screen() {
-        readarray -t screens <<< "$(xrandr \
-            | grep "\bconnected\b" \
-            | awk '{print $1, ($3 == "primary" ? 0 : 1)}' \
-            | sort -k2,2 -k1 \
-            | awk '{print $1}')"
+    screen_count=$(printf '%s\n' "${screens}" | wc -l )
 
-        local rofi_input_screens
-        for screen in "${screens[@]}"; do 
-            rofi_input_screens+="$(colored-icon pango 󰍹 ) ${screen}\n"
-        done 
+    if [ "${screen_count}" -eq 0 ]; then 
+        variant_screen=0
+    else 
+        rofi_input=$(printf '%s\n' "${screens}" \
+            | while read -r line; do 
+                printf '%s\n' "$(colored-icon pango 󰍹 ) ${line}"
+            done)
 
-        if (( ${#screens[@]} == 1 )); then 
-            local -r variant_screen=0
-        else 
-            local -r variant_screen=$(echo -en "${rofi_input_screens}" \
-                | rofi -config "${XDG_CONFIG_HOME}/rofi/dmenu-single-column.rasi" \
-                    -markup-rows -no-custom -i -dmenu \
-                    -format "i" \
-                    -p "Select screen" \
-                    -l ${#screens[@]})
-        fi
+        variant_screen=$(printf '%s\n' "${rofi_input}" \
+            | rofi -config "${XDG_CONFIG_HOME}/rofi/dmenu-single-column.rasi" \
+                -markup-rows -no-custom -i -dmenu \
+                -format "i" \
+                -p "Select screen" \
+                -l "${screen_count}")
+    fi
 
-        if [[ $variant_screen ]]; then 
-            echo "${screens[${variant_screen}]}"
-        fi
-    }
-
-    local -r variant=$(rofi_input \
-        | rofi -config "${XDG_CONFIG_HOME}/rofi/dmenu-single-column.rasi" \
-        -markup-rows -no-custom -i -dmenu \
-        -format "i" \
-        -p "󰹑 Screen capture:" \
-        -l 4)
-
-
-    if [[ $variant ]]; then
-        case $variant in
-            0) 
-                sleep 0.5
-                screen-capture-cmds focused 
-                ;;
-            1) 
-                local -r selected_screen="$(select_screen)"
-                sleep 0.5
-                screen-capture-cmds screen "${selected_screen}"
-                ;;
-            2) 
-                sleep 0.5
-                screen-capture-cmds select 
-                ;;
-            3)
-                if [[ ! -f ${XDG_RUNTIME_DIR}/ffmpeg.record.pid ]]; then 
-                    local -r selected_screen="$(select_screen)"
-                    sleep 0.5
-                    screen-capture-cmds record "${selected_screen}"
-                else 
-                    screen-capture-cmds record
-                fi
-                ;;
-            *) exit ;;
-        esac
+    if [ -n "$variant_screen" ]; then 
+        variant_screen=$((variant_screen + 1))
+        printf '%s\n' "${screens}" | sed -n "${variant_screen}p"
     fi
 }
 
-main "$@"
+rofi_str="\
+$(colored-icon pango 󰇀 ) Shot focused window
+$(colored-icon pango 󰍹 ) Shot whole screen
+$(colored-icon pango 󰒉 ) Shot selected area or window
+$(colored-icon pango  ) Record screen"
+
+variant=$(printf '%s\n' "${rofi_str}" \
+    | rofi -config "${XDG_CONFIG_HOME}/rofi/dmenu-single-column.rasi" \
+    -markup-rows -no-custom -i -dmenu \
+    -format "i" \
+    -p "󰹑 Screen capture:" \
+    -l 4)
+
+if [ -n "$variant" ]; then
+    case $variant in
+        0) 
+            sleep 0.5
+            screen-capture-cmds focused 
+            ;;
+        1) 
+            selected_screen="$(select_screen)"
+            sleep 0.5
+            screen-capture-cmds screen "${selected_screen}"
+            ;;
+        2) 
+            sleep 0.5
+            screen-capture-cmds select 
+            ;;
+        3)
+            if [ ! -f "${XDG_RUNTIME_DIR}/ffmpeg.record.pid" ]; then 
+                selected_screen="$(select_screen)"
+                sleep 0.5
+                screen-capture-cmds record "${selected_screen}"
+            else 
+                screen-capture-cmds record
+            fi
+            ;;
+        *) exit ;;
+    esac
+fi
+
