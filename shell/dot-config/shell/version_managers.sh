03321cbee5fa95bf7b4783c46a9e7c6e2ad339e9
b_chirva <bchirva@gmail.com>
2026-05-27 19:44:37 +0500

    Shell: load homebrew if exist

--- shell/dot-config/shell/version_managers.sh
+++ shell/dot-config/shell/version_managers.sh
@@ -1,5 +1,13 @@
+# Nvm
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  
 
-[ -d $PYENV_ROOT/bin ] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --detect-shell -)"
+# Pyenv
+if [ -d $PYENV_ROOT/bin ]; then
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --detect-shell -)"
+fi
+
+# Homebrew
+[ -s "/home/linuxbrew/.linuxbrew/bin/brew" ] && \
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv "$(ps -p $$ -o comm=)")"
