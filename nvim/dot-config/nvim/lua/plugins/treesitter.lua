87f74cec6be53b65efc6672ab2843c1034ab8f18
b_chirva <bchirva@gmail.com>
2026-06-13 22:50:00 +0500

    nvim: reconfigure nvim-treesitter plugin

--- nvim/dot-config/nvim/lua/plugins/treesitter.lua
+++ nvim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,24 @@
--- vim.pack.add({
---     src = "https://github.com/nvim-treesitter/nvim-treesitter",
---     version = "main"
--- })
---
--- build = ':TSUpdate'
+vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
+
+local treesitter = require("nvim-treesitter")
+treesitter.setup({
+    install_dir = vim.fn.stdpath("data") .. "/site"
+})
+
+treesitter.install({
+    "cpp",
+    "python",
+    "bash",
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function(ev)
+        if ev.data.spec.name == "nvim-treesitter"
+            and ev.data.kind == "update" then
+            if not ev.data.active then
+                vim.cmd.packadd("nvim-treesitter")
+            end
+            vim.cmd.TSUpdate()
+        end
+    end,
+})
